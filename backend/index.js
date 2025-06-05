3e50647de7efb879c96a9f8e4c56e981fbdd4aa3
Gabriela Silva <127990880+gabriela-silv@users.noreply.github.com>
2025-06-05 19:27:38 -0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,10 @@ const db = require("./db/db.js");
 const cors = require("cors");
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: 'http://3.148.216.231', // IP do seu frontend (ou domínio se tiver)
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+}));
 app.use(bodyParser.json()); // Para ler o corpo das requisições como JSON
 app.use("/api", taskRoutes);
 
